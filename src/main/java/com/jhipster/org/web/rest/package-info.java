7f4b283b2b3cda5beb08524449681d47c357bba5
Gabriel Borges <gabriel.borges@owse.com.br>
2023-10-27 19:03:29 -0300

    Initial version of jhipster generated by generator-jhipster@7.9.4

---
+++ src/main/java/com/jhipster/org/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.jhipster.org.web.rest;
---
+++ src/main/java/com/jhipster/org/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.jhipster.org.web.rest.vm;
